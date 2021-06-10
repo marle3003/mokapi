c5b3ce3efc78a5a0f1c37a092fa9eba3c7083f57
marle3003 <56543258+marle3003@users.noreply.github.com>
2021-06-10 10:22:10 +0200

    added error handling

--- providers/encoding/json.go
+++ providers/encoding/json.go
@@ -17,7 +17,10 @@ func UnmarshalJSON(s string, schema *openapi.SchemaRef) (interface{}, error) {
 }
 
 func MarshalJSON(obj interface{}, schema *openapi.SchemaRef) ([]byte, error) {
-	data := selectData(obj, schema)
+	data, err := selectData(obj, schema)
+	if err != nil {
+		return nil, err
+	}
 	return json.Marshal(data)
 }
 
@@ -48,35 +51,39 @@ func (m custom) MarshalJSON() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func selectData(data interface{}, schema *openapi.SchemaRef) interface{} {
+func selectData(data interface{}, schema *openapi.SchemaRef) (interface{}, error) {
 	if schema.Value == nil || schema.Value.Type == "" {
-		return data
+		return data, nil
 	}
 
 	if data == nil {
-		return nil
+		return nil, nil
 	}
 
 	switch data.(type) {
 	case []interface{}:
 		if schema.Value.Type != "array" {
-			return fmt.Errorf("expected %q but found array", schema.Value.Type)
+			return nil, fmt.Errorf("expected %q but found array", schema.Value.Type)
 		}
 	case map[string]interface{}:
 		if schema.Value.Type != "object" {
-			return fmt.Errorf("expected %q but found object", schema.Value.Type)
+			return nil, fmt.Errorf("expected %q but found object", schema.Value.Type)
 		}
 	}
 
 	if schema.Value.Type == "array" {
 		if list, ok := data.([]interface{}); ok {
 			for i, e := range list {
-				list[i] = selectData(e, schema.Value.Items)
+				var err error
+				list[i], err = selectData(e, schema.Value.Items)
+				if err != nil {
+					return nil, err
+				}
 			}
-			return list
+			return list, nil
 		}
-		// todo error handling
-		return nil
+
+		return nil, fmt.Errorf("unexpected type for schema type array")
 	} else if schema.Value.Type == "object" {
 		var obj map[string]interface{}
 		if o, isObject := data.(map[string]interface{}); isObject {
@@ -85,25 +92,32 @@ func selectData(data interface{}, schema *openapi.SchemaRef) interface{} {
 			s := fmt.Sprintf("%v", data)
 			err := json.Unmarshal([]byte(s), &obj)
 			if err != nil {
-				// todo error handling
-				return nil
+				return nil, err
 			}
 		}
 
 		if schema.Value.Properties == nil || len(schema.Value.Properties.Value) == 0 {
-			return obj
+			return obj, nil
 		}
 
 		selectedData := make(custom)
 
 		for k, v := range obj {
 			if p, ok := schema.Value.Properties.Value[k]; ok {
-				selectedData[k] = selectData(v, p)
+				var err error
+				selectedData[k], err = selectData(v, p)
+				if err != nil {
+					return nil, err
+				}
 			} else if schema.Value.AdditionalProperties != nil {
-				selectedData[k] = selectData(v, schema.Value.AdditionalProperties)
+				var err error
+				selectedData[k], err = selectData(v, schema.Value.AdditionalProperties)
+				if err != nil {
+					return nil, err
+				}
 			}
 		}
-		return selectedData
+		return selectedData, nil
 	}
-	return data
+	return data, nil
 }
