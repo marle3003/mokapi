cac4f950c98edeb4f5e8c3f6f1886faf462d2944
marle3003 <56543258+marle3003@users.noreply.github.com>
2021-10-01 15:45:18 +0200

    extended log package

--- lua/log/log.go
+++ lua/log/log.go
@@ -35,15 +35,47 @@ func (l *Log) Debug(s string) {
 	}
 }
 
-func (l *Log) Loader(state *lua.LState) int {
-	debug := func(L *lua.LState) int {
-		s := L.ToString(1)
-		l.Debug(s)
-		return 0
+func (l *Log) Info(s string) {
+	log.Info(s)
+	if l.Handler != nil && l.Level >= InfoLevel {
+		l.Handler(fmt.Sprintf("info: %v", s))
 	}
+}
 
+func (l *Log) Warn(s string) {
+	log.Warn(s)
+	if l.Handler != nil && l.Level >= WarnLevel {
+		l.Handler(fmt.Sprintf("warn: %v", s))
+	}
+}
+
+func (l *Log) Error(s string) {
+	log.Error(s)
+	l.Handler(fmt.Sprintf("error: %v", s))
+}
+
+func (l *Log) Loader(state *lua.LState) int {
 	exports := map[string]lua.LGFunction{
-		"debug": debug,
+		"debug": func(L *lua.LState) int {
+			s := L.ToString(1)
+			l.Debug(s)
+			return 0
+		},
+		"info": func(L *lua.LState) int {
+			s := L.ToString(1)
+			l.Info(s)
+			return 0
+		},
+		"warn": func(L *lua.LState) int {
+			s := L.ToString(1)
+			l.Warn(s)
+			return 0
+		},
+		"error": func(L *lua.LState) int {
+			s := L.ToString(1)
+			l.Error(s)
+			return 0
+		},
 	}
 
 	mod := state.SetFuncs(state.NewTable(), exports)
