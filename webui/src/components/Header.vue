b7c99402d20072b1eceae41caaa6b39c16454fbf
maesi <marcel@famlehmann.ch>
2023-04-01 16:03:41 +0200

    updated website

--- webui/src/components/Header.vue
+++ webui/src/components/Header.vue
@@ -3,10 +3,14 @@ import { useAppInfo } from '../composables/appInfo'
 import { RouterLink, useRouter } from 'vue-router'
 import { onUnmounted } from 'vue';
 
-const appInfo = useAppInfo()
-onUnmounted(() => {
-    appInfo.close()
-})
+const isDashboardEnabled = import.meta.env.VITE_DASHBOARD == 'true'
+
+if (isDashboardEnabled) {
+  const appInfo = useAppInfo()
+  onUnmounted(() => {
+      appInfo.close()
+  })
+}
 
 const isDark = document.documentElement.getAttribute('data-theme') == 'dark';
 
@@ -17,14 +21,13 @@ function switchTheme() {
   localStorage.setItem('theme', theme)
   router.go(0)
 }
-const isDashboardEnabled = import.meta.env.VITE_DASHBOARD == 'true'
 </script>
 
 <template>
   <header>
     <nav class="navbar navbar-expand-lg ">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/" > <img src="/public/logo-header.svg" height="30" /></a>
+        <a class="navbar-brand" href="/"><img src="/public/logo-header.svg" height="30" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -38,7 +41,7 @@ const isDashboardEnabled = import.meta.env.VITE_DASHBOARD == 'true'
             </li>
           </ul>
 
-          <div class="d-flex ms-auto tools">
+          <div class="d-flex ms-auto tools" v-if="isDashboardEnabled">
             
             <a href="https://github.com/marle3003/mokapi" class="version" v-if="appInfo.data">Version {{appInfo.data.version}}</a>
             <i class="bi bi-brightness-high-fill" @click="switchTheme" v-if="isDark"></i>
