0d81070f30c315a843915d8a4c471d8531ec0761
maesi <marcel@famlehmann.ch>
2023-04-17 22:13:06 +0200

    added home view to sitemap.xml

--- webui/build-sitemap.js
+++ webui/build-sitemap.js
@@ -3,6 +3,11 @@ const util = require('util');
 
 const xmlTemplate = `
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<url>
+  <loc>https://mokapi.io/home/</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.8</priority>
+</url>
 %s
 </urlset>
 `
