e86515d41a50076e8ea5c67920b957c62e02badc
maesi <marcel@famlehmann.ch>
2023-06-02 19:46:17 +0200

    fix test

--- webui/e2e/home.spec.ts
+++ webui/e2e/home.spec.ts
@@ -4,5 +4,5 @@ test('home overview', async ({ home, page }) => {
     await home.open()
 
     await expect(home.heroTitle).toHaveText('Easy and flexible API mocking')
-    await expect(home.heroDescription).toHaveText(`Simplify your test workflows and accelerate your development`)
+    await expect(home.heroDescription).toHaveText(`Simplify your test workflows to build better software faster`)
 })
