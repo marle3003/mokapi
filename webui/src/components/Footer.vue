9fe32f41c8d66058907b7c5ac06f086b80c49b83
maesi <marcel@famlehmann.ch>
2023-06-03 23:35:49 +0200

    update text add images fixed npm package for macos

---
+++ webui/src/components/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-2">
+          <img src="/logo.svg" height="120" alt="Logo Mokapi"/>
+        </div>
+        <div class="col-2">
+          <div class="row">
+            <div class="col">
+              <h3>Guides</h3>
+              <ul>
+                <li>
+                  <router-link :to="{ path: '/docs/guides/get-started/installation' }">
+                    Installation
+                  </router-link>
+                </li>
+                <li>
+                  <router-link :to="{ path: '/docs/guides/get-started/running' }">
+                    Running
+                  </router-link>
+                </li>
+                <li>
+                  <router-link :to="{ path: '/docs/guides/get-started/test-data' }">
+                    Test Data
+                  </router-link>
+                </li>
+                <li>
+                  <router-link :to="{ path: '/docs/guides/get-started/dashboard' }">
+                    Dashboard
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="row">
+            <div class="col">
+              <h3>Examples</h3>
+              <ul>
+                <li>
+                  <router-link :to="{ path: '/docs/examples/simple-http-api' }">
+                    OpenAPI with Mokapi Script
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="row">
+            <div class="col">
+              <h3>Links</h3>
+              <ul>
+                <li>
+                  <a href="https://github.com/marle3003/mokapi">
+                    Docker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.npmjs.com/package/go-mokapi">
+                    NPM
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/marle3003/mokapi">
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { useMeta } from '@/composables/meta'
+import Footer from '@/components/Footer.vue'
+import { ref, onMounted } from 'vue'
+import { Modal } from 'bootstrap'
 
 const script = `import { on } from 'mokapi'
 
@@ -16,13 +19,29 @@ export default function() {
 const description = `Mokapi is an open-source tool that makes mock up easy for developers and QA engineers`
 
 useMeta('Mocking tool for engineering teams | mokapi.io', description, 'https://mokapi.io')
+
+const dialog = ref<Modal>()
+const imageUrl = ref<string>()
+
+onMounted(() => {
+  dialog.value = new Modal('#imageDialog', {})
+})
+
+function showImage(target: EventTarget | null) {
+  if (!target) {
+    return
+  }
+  const element = target as HTMLImageElement
+  imageUrl.value = element.src
+  dialog.value?.show()
+}
 </script>
 
 <template>
   <main class="home">
     <section>
       <div class="container">
-        <div class="row hero-title">
+        <div class="row hero-title justify-content-center">
           <div class="col-12 col-lg-6">
             <h1>Easy and flexible API mocking </h1>
             <p class="description">Simplify your test workflows to build better software faster</p>
@@ -35,8 +54,8 @@ useMeta('Mocking tool for engineering teams | mokapi.io', description, 'https://
               </router-link>
             </p>
           </div>
-          <div class="col-12 col-lg-6 justify-content-center">
-            <img src="/logo.svg" alt="Logo Mokapi" />
+          <div class="col-12 col-lg-5">
+            <img src="/logo.svg" alt="Logo Mokapi" class="mx-auto d-block" />
           </div>
           <div class="col-12 d-block d-md-none">
             <p style="margin-top: 2rem;">
@@ -91,6 +110,91 @@ useMeta('Mocking tool for engineering teams | mokapi.io', description, 'https://
         </div>
       </div>
     </section>
+    <section class="feature">
+      <div class="container">
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            <pre v-highlightjs="script"><code class="javascript"></code></pre>
+          </div>
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Control Everything</h3>
+            <p>Write the behavior of your mock in Javascript and control everything, such as delay, errors or other edge cases.</p>
+            <router-link :to="{ path: '/docs/javascript-api' }">
+              <button type="button" class="btn btn-outline-primary btn-sm">Javascript API</button>
+            </router-link>
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Run Anywhere</h3>
+            <p>Use the <a href="https://hub.docker.com/r/mokapi/mokapi">Docker image</a>, the <a href="https://www.npmjs.com/package/go-mokapi">NPM package</a> or directly as an <a href="https://github.com/marle3003/mokapi/releases">executable</a>.</p>
+            <router-link :to="{ path: '/docs/guides/get-started/running' }">
+              <button type="button" class="btn btn-outline-primary btn-sm">Read more</button>
+            </router-link>
+          </div>
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Everything as Code</h3>
+            <p>Get all benefits of everything as code such as version control and consistency</p>
+          </div>
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            <img src="systemtest.png" @click="showImage($event.target)" />
+          </div>
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>End-to-End Tests</h3>
+            <p>Test only system that you are responsible and fake all others with Mokapi.</p>
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>See what's going on</h3>
+            <p>Quickly analyze and inspect all requests and responses in the dashboard to gather insights on how your mock APIs are used.</p>
+            <router-link :to="{ path: '/docs/guides/get-started/dashboard' }">
+              <button type="button" class="btn btn-outline-primary btn-sm">Read more</button>
+            </router-link>
+          </div>
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            <img src="systemcontracts.png" style="max-height:75%" @click="showImage($event.target)" />
+          </div>
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Contract Testing</h3>
+            <p>Fake interactions under test control between your software and systems that it collaborates.</p>
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Build better software faster</h3>
+            <p>Good tests are deterministic and durable. Test your software if it is ready to release. Only your software, nothing else.</p>
+          </div>
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            
+          </div>
+        </div>
+        <div class="row pb-6">
+          <div class="col-12 col-lg-6 ps-5 pe-3">
+            
+          </div>
+          <div class="col-12 col-lg-6 ps-3 pe-5 pt-5">
+            <h3>Open Source</h3>
+            <p>Mokapi is a completely open source and free API mocking tool (<a href="https://github.com/marle3003/mokapi">GitHub Repo</a>)</p>
+          </div>
+        </div>
+      </div>
+    </section>
     <section>
       <div class="container">
         <div class="row">
@@ -114,8 +218,20 @@ useMeta('Mocking tool for engineering teams | mokapi.io', description, 'https://
       </div>
     </section>
   </main>
+  <Footer></Footer>
+  <div class="modal fade" id="imageDialog" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-body">
+          <img :src="imageUrl" style="width:100%" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.pb-6 {
+  padding-bottom: 5rem;
+}
 </style>
