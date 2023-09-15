50ca873c20980efeb90a0bc6157b82018326d161
maesi <marcel@famlehmann.ch>
2023-09-15 08:12:17 +0200

    fix test

--- webui/e2e/home.spec.ts
+++ webui/e2e/home.spec.ts
@@ -4,5 +4,5 @@ test('home overview', async ({ home, page }) => {
     await home.open()
 
     await expect(home.heroTitle).toHaveText('Easy and flexible API mocking')
-    await expect(home.heroDescription).toHaveText(`Mock your APIs in Seconds - Free and Open-Source`)
+    await expect(home.heroDescription).toHaveText(`Mock your APIs in Seconds - No registration, free and open-source`)
 })
