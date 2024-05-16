208738f14f75ea0cac2dc3fccc5e6e08fdf4dff4
maesi <marcel@famlehmann.ch>
2024-05-16 11:18:49 +0200

    add tests

--- config/dynamic/config_info_test.go
+++ config/dynamic/config_info_test.go
@@ -45,6 +45,83 @@ func TestConfigInfo(t *testing.T) {
 				require.Greater(t, cfg.Info.Time, now)
 			},
 		},
+		{
+			name: "get Key when url is defined",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{
+					Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("https://mokapi.io")),
+				}
+				require.Equal(t, "61633037-3161-3264-6332-653235343933", cfg.Info.Key())
+			},
+		},
+		{
+			name: "get Key when url is not defined",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{}
+				require.Equal(t, "", cfg.Info.Key())
+			},
+		},
+		{
+			name: "kernel gets itself if no nested set",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{}
+				require.Equal(t, cfg.Info, *cfg.Info.Kernel())
+			},
+		},
+		{
+			name: "kernel gets nested info",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("file://inner.yaml"))}
+				origin := cfg.Info
+				outer := dynamictest.NewConfigInfo(dynamictest.WithUrl("foo.yaml"))
+				dynamic.Wrap(outer, cfg)
+				require.Equal(t, origin.Key(), cfg.Info.Kernel().Key())
+			},
+		},
+		{
+			name: "match url wrapped config",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("file://inner.yaml"))}
+				outer := dynamictest.NewConfigInfo(dynamictest.WithUrl("foo.yaml"))
+				dynamic.Wrap(outer, cfg)
+
+				u, _ := url.Parse("file://inner.yaml")
+				require.True(t, cfg.Info.Match(u))
+			},
+		},
+		{
+			name: "match url",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("file://inner.yaml"))}
+				outer := dynamictest.NewConfigInfo(dynamictest.WithUrl("foo.yaml"))
+				dynamic.Wrap(outer, cfg)
+
+				u, _ := url.Parse("foo.yaml")
+				require.True(t, cfg.Info.Match(u))
+			},
+		},
+		{
+			name: "match inner url",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("file://inner.yaml"))}
+				outer := dynamictest.NewConfigInfo(dynamictest.WithUrl("foo.yaml"))
+				dynamic.Wrap(outer, cfg)
+
+				u, _ := url.Parse("file://inner.yaml")
+				require.True(t, cfg.Info.Match(u))
+			},
+		},
+		{
+			name: "not matching",
+			test: func(t *testing.T) {
+				cfg := &dynamic.Config{Info: dynamictest.NewConfigInfo(dynamictest.WithUrl("file://inner.yaml"))}
+				outer := dynamictest.NewConfigInfo(dynamictest.WithUrl("foo.yaml"))
+				dynamic.Wrap(outer, cfg)
+
+				u, _ := url.Parse("other.yaml")
+				require.False(t, cfg.Info.Match(u))
+			},
+		},
 	}
 
 	t.Parallel()
