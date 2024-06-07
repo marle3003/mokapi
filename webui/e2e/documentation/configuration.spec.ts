8d3eac898ed96e331400243e38f5f636c272092d
maesi <marcel@famlehmann.ch>
2024-06-07 20:10:58 +0200

    fix e2e test

--- webui/e2e/documentation/configuration.spec.ts
+++ webui/e2e/documentation/configuration.spec.ts
@@ -15,27 +15,27 @@ test('Visit Configuration', async ({ page, home }) => {
     })
 
     await test.step('navigation section providers', async () => {
-        const region = page.getByRole('region', { name: 'Providers' })
+        const region = page.getByRole('region', { name: 'Dynamic' })
         const link = page.getByRole('link', { name: 'File' })
         await expect(region).toBeVisible()
         await expect(link).toBeVisible()
-        await page.getByRole('button', { name: 'Providers' }).click()
+        await page.getByRole('button', { name: 'Dynamic' }).click()
         await expect(region).not.toBeVisible()
         await expect(link).not.toBeVisible()
     })
 
     await test.step('Visit File', async () => {
-        await page.getByRole('button', { name: 'Providers' }).click()
+        await page.getByRole('button', { name: 'Dynamic' }).click()
         await page.getByRole('link', { name: 'File' }).click()
 
         await test.step('meta information are available', async () => {
-            await expect(page).toHaveURL('/docs/configuration/providers/file')
+            await expect(page).toHaveURL('/docs/configuration/dynamic/file')
             await expect(page).toHaveTitle('File Provider | Mokapi Configuration')
             await expect(page.locator('meta[name="description"]')).toHaveAttribute(
                 'content',
                 'The file provider reads dynamic configuration from a single file or multiple files.'
             )
-            await expect(page.locator('link[rel="canonical"]')).toHaveAttribute('href', 'https://mokapi.io/docs/configuration/providers/file')
+            await expect(page.locator('link[rel="canonical"]')).toHaveAttribute('href', 'https://mokapi.io/docs/configuration/dynamic/file')
         })
 
     })
