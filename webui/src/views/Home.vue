d447b53fa409c300900bb280df3ed968cfd0e585
maesi <marcel@famlehmann.ch>
2024-08-13 23:21:31 +0200

    update image alt text

--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -51,7 +51,7 @@ function showImage(target: EventTarget | null) {
             </p>
           </div>
           <div class="col-12 col-lg-5">
-            <img src="/logo.svg" alt="Mokapi logo - Mockapi became an Okapi" class="mx-auto d-block" />
+            <img src="/logo.svg" alt="Mokapi" class="mx-auto d-block" />
           </div>
           <div class="col-12 d-block d-md-none">
             <p style="margin-top: 2rem;">
@@ -123,7 +123,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-5 pe-lg-3 d-flex align-items-center order-lg-1">
-            <img src="/controleverything.png" @click="showImage($event.target)" alt="Script shows how to control everything with JavaScript in Mokapi" />
+            <img src="/controleverything.png" @click="showImage($event.target)" alt="A JavaScript code that shows how easy it is to realistically mock an API and control everything." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5 mb-lg-5 mt-lg-5">
@@ -140,7 +140,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-3 pe-lg-5 d-flex align-items-center order-lg-2">
-            <img src="/anywhere.png" @click="showImage($event.target)" alt="Shows logs in a console containing Docker and NodeJS logos where Mokapi can be run." />
+            <img src="/anywhere.png" @click="showImage($event.target)" alt="Log output starting Mokapi in a Docker image." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5mb-lg-5 mt-lg-5">
@@ -154,7 +154,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-5 pe-lg-3 d-flex align-items-center order-lg-1">
-            <img src="/everythingcode.png" @click="showImage($event.target)" alt="Shows scripts ans configuration file to demostrate feature Everything as Code." />
+            <img src="/everythingcode.png" @click="showImage($event.target)" alt="Example OpenAPI specification, a script to generate Kafka mock data and forward emails sent to a mock SMTP server." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5 mb-lg-5 mt-lg-5">
@@ -171,7 +171,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-3 pe-lg-5 order-lg-2">
-            <img src="/faker-tree.png" @click="showImage($event.target)" alt="Shows a section of Mokapi's Decision Tree to generate useful random data."/>
+            <img src="/faker-tree.png" @click="showImage($event.target)" alt="A part of Mokapi's Faker decision tree for generating realistic random data depending on the data structure."/>
           </div>
         </div>
         <div class="row pb-4 pb-lg-5 mb-lg-5 mt-lg-5">
@@ -185,7 +185,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-3 pe-lg-5 order-lg-1">
-            <img src="/patching.png" @click="showImage($event.target)" alt="Shows an example of patching that allows more flexibility in mocking APIs" />
+            <img src="/patching.png" @click="showImage($event.target)" alt="The original OpenAPI specification is patched with two additional files at runtime." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5 mb-lg-5 mt-lg-5">
@@ -201,7 +201,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-3 pe-lg-5 order-lg-2">
-            <img src="/systemtest.png" @click="showImage($event.target)" alt="Mokapi helps you to simulate interfaces like REST API or Apache Kafka and allows to bring the really important information into the system under test." />
+            <img src="/systemtest.png" @click="showImage($event.target)" alt="A variant of how Mokapi can be used to test a system with realistic data." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5 mb-lg-5 mt-lg-5">
@@ -215,7 +215,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-5 pe-lg-3 d-flex align-items-center order-lg-1">
-            <img src="/dashboard.png" @click="showImage($event.target)" alt="Shows a screenshot of Mokapi's dashboard with an overview of all mocked services." />
+            <img src="/dashboard.png" @click="showImage($event.target)" alt="Mokapi's dashboard with an overview of all faked services including metrics and logs." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5">
@@ -229,7 +229,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-3 pe-lg-5 d-flex align-items-center order-lg-2">
-            <img src="/systemcontracts.png" @click="showImage($event.target)" alt="Test only system that we are responsible for and fake all others" />
+            <img src="/systemcontracts.png" @click="showImage($event.target)" alt="A contract test with Mokapi to test edge cases that are difficult to produce in a real environment." />
           </div>
         </div>
         <div class="row pb-4 pb-lg-5">
@@ -240,7 +240,7 @@ function showImage(target: EventTarget | null) {
             </div>
           </div>
           <div class="col-12 col-lg-6 ps-lg-5 pe-lg-3 order-lg-1">
-            <img src="/betterfaster.png" class="float-lg-end mx-auto d-block" style="width: 80%" @click="showImage($event.target)" alt="Improve speed, accuracy and efficiency of testing and deliver higher quality" />
+            <img src="/betterfaster.png" class="float-lg-end mx-auto d-block" style="width: 80%" @click="showImage($event.target)" alt="Mokapi can support you in both the development and testing process" />
           </div>
         </div>
       </div>
