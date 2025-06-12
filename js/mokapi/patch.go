cf9f97d20263dc669e5f59b676b69c341c747dae
maesi <marcel@famlehmann.ch>
2025-06-12 15:50:31 +0200

    fix patching array list

--- js/mokapi/patch.go
+++ js/mokapi/patch.go
@@ -16,7 +16,13 @@ func patch(target, patch any) any {
 	if isTargetMap && isPatchMap {
 		return patchMap(mapTarget, mapPatch)
 	} else {
-		return patch
+		arrTarget, isTargetArray := target.([]any)
+		arrPatch, isPatchArray := patch.([]any)
+		if isTargetArray && isPatchArray {
+			return patchArray(arrTarget, arrPatch)
+		} else {
+			return patch
+		}
 	}
 }
 
@@ -40,5 +46,28 @@ func patchMap(t, p map[string]any) map[string]any {
 			result[k] = v
 		}
 	}
+	return result
+}
+
+func patchArray(t, p []any) []any {
+	result := make([]any, 0, len(p))
+
+	// copy original value
+	for _, v := range t {
+		result = append(result, v)
+	}
+
+	for i, v := range p {
+		if v == Delete {
+			result = append(result[:i], result[i+1:]...)
+			continue
+		}
+		if i < len(result) {
+			result[i] = patch(result[i], v)
+		} else {
+			result = append(result, v)
+		}
+	}
+
 	return result
 }
