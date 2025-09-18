585d2bbe3a34fc1f02b90bc6e0e2b52bb9215049
maesi <marcel@famlehmann.ch>
2025-09-18 19:04:58 +0200

    add test

--- engine/common/http_test.go
+++ engine/common/http_test.go
@@ -27,3 +27,39 @@ func TestEventResponse_HasBody(t *testing.T) {
 	r.Data = 123
 	require.True(t, r.HasBody())
 }
+
+func TestHttpResource(t *testing.T) {
+	testcases := []struct {
+		url      Url
+		resource any
+		test     func(t *testing.T, b bool, body any, err error)
+	}{
+		{
+			url: Url{
+				Path: "/foo",
+			},
+			resource: map[string]any{
+				"foo": "bar",
+			},
+			test: func(t *testing.T, b bool, body any, err error) {
+				require.NoError(t, err)
+				require.True(t, b)
+				require.Equal(t, "bar", body)
+			},
+		},
+	}
+
+	t.Parallel()
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.url.String(), func(t *testing.T) {
+			t.Parallel()
+			req := &EventRequest{
+				Url: tc.url,
+			}
+			res := &EventResponse{}
+			b, err := EventHandler(req, res, tc.resource)
+			tc.test(t, b, res.Data, err)
+		})
+	}
+}
