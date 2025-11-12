0cc96d2a932f704aa60c51bfa13f6b8a77df6d93
maesi <marcel@famlehmann.ch>
2025-11-12 12:54:23 +0100

    fix faker

--- js/faker/faker.go
+++ js/faker/faker.go
@@ -3,6 +3,7 @@ package faker
 import (
 	"fmt"
 	"mokapi/engine/common"
+	"mokapi/js/eventloop"
 	"mokapi/js/util"
 	"mokapi/providers/openapi/schema"
 	"mokapi/schema/json/generator"
@@ -14,24 +15,53 @@ import (
 type Module struct {
 	vm   *goja.Runtime
 	host common.Host
+	loop *eventloop.EventLoop
 }
 
 func Require(rt *goja.Runtime, module *goja.Object) {
 	o := rt.Get("mokapi/internal").(*goja.Object)
 	host := o.Get("host").Export().(common.Host)
+	loop := o.Get("loop").Export().(*eventloop.EventLoop)
 	f := &Module{
 		vm:   rt,
 		host: host,
+		loop: loop,
 	}
 	obj := module.Get("exports").(*goja.Object)
 	_ = obj.Set("fake", f.Fake)
+	_ = obj.Set("fakeAsync", f.FakeAsync)
 	_ = obj.Set("findByName", f.FindByName)
 	_ = obj.Set("ROOT_NAME", generator.RootName)
 }
 
 func (m *Module) Fake(v goja.Value) interface{} {
+	i, err := m.fake(v)
+	if err != nil {
+		panic(m.vm.ToValue(err.Error()))
+	}
+	return i
+}
+
+func (m *Module) FakeAsync(v goja.Value) *goja.Promise {
+	p, resolve, reject := m.vm.NewPromise()
+	go func() {
+		i, err := m.fake(v)
+		if err != nil {
+			m.loop.Run(func(vm *goja.Runtime) {
+				_ = reject(err)
+			})
+		} else {
+			m.loop.Run(func(vm *goja.Runtime) {
+				_ = resolve(i)
+			})
+		}
+	}()
+	return p
+}
+
+func (m *Module) fake(v goja.Value) (interface{}, error) {
 	if v == nil {
-		return nil
+		return nil, nil
 	}
 
 	t := v.ExportType()
@@ -54,9 +84,5 @@ func (m *Module) Fake(v goja.Value) interface{} {
 		r.Schema = s
 	}
 
-	i, err := generator.New(r)
-	if err != nil {
-		panic(m.vm.ToValue(err.Error()))
-	}
-	return i
+	return generator.New(r)
 }
