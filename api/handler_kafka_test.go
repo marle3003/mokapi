e8a1ad1fc3dcb2f3b6dc3acf4f05631b397e63bd
maesi <marcel@famlehmann.ch>
2025-12-01 19:39:55 +0100

    fix test

--- api/handler_kafka_test.go
+++ api/handler_kafka_test.go
@@ -695,7 +695,7 @@ func TestHandler_KafkaAPI(t *testing.T) {
 
 				require.Equal(t, float64(1), app.Monitor.Kafka.Messages.WithLabel("foo", "topic-1").Value())
 				p := app.Kafka.Get("foo").Store.Topic("topic-1").Partition(0)
-				require.Equal(t, `{"foo": "bar"}`, string(kafka.Read(p.Segments[p.ActiveSegment].Log[0].Data.Value)))
+				require.Equal(t, `{"foo":"bar"}`, string(kafka.Read(p.Segments[p.ActiveSegment].Log[0].Data.Value)))
 			},
 		},
 		{
