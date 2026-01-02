e44acfd4c0d3aaa9072643fdc4493ebd87f77343
maesi <marcel@famlehmann.ch>
2026-01-02 18:52:33 +0100

    add more HTTP requests to demo dashboard

--- webui/scripts/dashboard-demo/demo-configs/petstore.ts
+++ webui/scripts/dashboard-demo/demo-configs/petstore.ts
@@ -4,7 +4,6 @@ import { fake } from 'mokapi/faker'
 export default function() {
     on('http', (request, response) => {
         if (request.operationId === 'getUserByName') {
-
             const username = request.path.username;
             console.log(`[HTTP] Incoming request → GET /user/${username}`);
             console.log(`[HTTP] Operation matched: getUserByName`);
@@ -18,10 +17,67 @@ export default function() {
                 response.statusCode = 404
             }
         }
+        switch (request.key) {
+            case '/user/login': {
+                const username = request.query.username;
+                if (users.has(username)) {
+                    const user = users.get(username);
+                    console.log(`[Script] User ${username} found`)
+                    if (user.password === request.query.password) {
+                        response.data = 'ok'
+                    } else {
+                        console.log(`[Script] Password for ${username} is invalid`)
+                        response.statusCode = 400;
+                    }
+                } else {
+                    console.log(`[Script] User ${username} not found`)
+                    response.statusCode = 400;
+                }
+                break;
+            }
+            case '/store/order': {
+                orders.set(request.body.id, request.body);
+                response.data = request.body;
+                break;
+            }
+            case '/store/order/{orderId}': {
+                if (request.method === 'GET') {
+                    const order = orders.get(request.path.orderId);
+                    if (order) {
+                        response.data = order;
+                    } else {
+                        response.statusCode = 404;
+                    }
+                } else if (request.method === 'DELETE') {
+                    const orderId = request.path.orderId;
+                    if (orders.delete(orderId)) {
+                        response.statusCode = 204;
+                    } else {
+                        response.statusCode = 404;
+                    }
+                }
+                break;
+            }
+            case '/user/logout': {
+                // force a script error with undefined variable user
+                console.log(`logout current user ${user}`)
+                break;
+            }
+        }
     })
 }
 
 const users = new Map([
+    ['ajohnson', {
+        id: fake({ type: 'integer', format: 'int64' }),
+        username: 'ajohnson',
+        firstName: 'Alice',
+        lastName: 'Johnson',
+        email: 'alice.johnson@example.com',
+        password: 'anothersecretpassword456',
+        phone: fake({ type: 'string', pattern: '(?:(?:\\+|0{0,2})91(\\s*[\\- ]\\s*)?|[0 ]?)?[789]\\d{9}|(\\d[ -]?){10}\\d' }),
+        userStatus: 1
+    }],
     ['bmiller', {
         id: fake({ type: 'integer', format: 'int64' }),
         username: 'bmiller',
@@ -33,3 +89,12 @@ const users = new Map([
         userStatus: 1
     }]
 ]);
+
+const orders = new Map([
+    [1, {
+        petId: 74959,
+        quantity: 1,
+        shipDate: '2011-01-08T16:59:15Z',
+        status: 'delivered'
+    }]
+])
--- webui/scripts/dashboard-demo/drive-http.ts
+++ webui/scripts/dashboard-demo/drive-http.ts
@@ -1,5 +1,5 @@
 export async function driveHttp() {
-  await fetch('http://localhost/v2/pet/10', { headers: { api_key: 'demo' }})
+  await fetch('http://localhost/v2/pet/10', { headers: { Accept: 'application/json', api_key: 'demo' }})
   await fetch('http://localhost/v2/pet', {
     method: 'POST',
     body: JSON.stringify({ name: 'Milo', photoUrls: [] }),
@@ -10,4 +10,42 @@ export async function driveHttp() {
   });
 
   await fetch('http://localhost/v2/user/bmiller');
+
+  // invalid request without password
+  await fetch('http://localhost/v2/user/login?username=ajohnson');
+  // valid
+  await fetch('http://localhost/v2/user/login?username=ajohnson&password=anothersecretpassword456');
+  // invalid password
+  await fetch('http://localhost/v2/user/login?username=bmiller&password=12345');
+  await fetch('http://localhost/v2/user/login?username=bmiller&password=mysecretpassword123');
+  // request with script error
+  await fetch('http://localhost/v2/user/login?username=bmiller&password=mysecretpassword123');
+  // script error
+  await fetch('http://localhost/v2/user/logout');
+
+  // should only return pets with the given status.
+  await fetch('http://localhost/v2/pet/findByStatus?status=available,pending', {
+    headers: {
+      Accept: 'application/xml',
+      Authorization: 'demo'
+    }
+  });
+  
+  await fetch('http://localhost/v2/store/order/1')
+  await fetch('http://localhost/v2/store/order', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: 2,
+      petId: 14921,
+      quantity: 2,
+      status: 'placed'
+    })
+  })
+  await fetch('http://localhost/v2/store/order/2', {
+    method: 'DELETE',
+  })
 }
