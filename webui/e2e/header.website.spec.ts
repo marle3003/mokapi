5bfee9105c0571f3909450f54a4d27a1ecca93c4
maesi <marcel@famlehmann.ch>
2026-02-10 19:02:32 +0100

    fix test

--- webui/e2e/header.website.spec.ts
+++ webui/e2e/header.website.spec.ts
@@ -1,16 +1,17 @@
-import { test, expect } from './models/fixture-dashboard'
+import { test, expect } from './models/fixture-dashboard';
 
 test('header in dashboard', async ({ page }) => {
-    await page.goto('/home')
+    await page.goto('/home');
 
     await test.step("navigation links", async () => {
         const links = page.getByRole('banner').getByRole('navigation').getByRole('link');
-        await expect(links.nth(0)).toHaveAccessibleDescription('Mokapi home')
-        await expect(links.nth(1)).toHaveText('Dashboard')
-        await expect(links.nth(2)).toHaveText('Guides')
-        await expect(links.nth(3)).toHaveText('Configuration')
-        await expect(links.nth(4)).toHaveText('JavaScript API')
-        await expect(links.nth(5)).toHaveText('Resources')
-        await expect(links.nth(6)).toHaveText('References')
+        await expect(links.nth(0)).toHaveAccessibleDescription('Mokapi home');
+        await expect(links.nth(1)).toHaveText('HTTP');
+        await expect(links.nth(2)).toHaveText('Kafka');
+        await expect(links.nth(3)).toHaveText('LDAP');
+        await expect(links.nth(4)).toHaveText('Mail');
+        await expect(links.nth(5)).toHaveText('Dashboard');
+        await expect(links.nth(6)).toHaveText('Docs');
+        await expect(links.nth(7)).toHaveText('Resources');
     })
 })
