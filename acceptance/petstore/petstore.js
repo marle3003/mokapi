02df9634d4aadd60df4c047591151323ca96cab5
maesi <marcel@famlehmann.ch>
2026-06-21 15:16:36 +0200

    fix: remove old return style

--- acceptance/petstore/petstore.js
+++ acceptance/petstore/petstore.js
@@ -3,32 +3,30 @@ import kafka from 'mokapi/kafka'
 
 export default async function() {
     on('kafka', function (record) {
-        record.headers = { foo: 'bar' }
-        return true
+        record.headers = { foo: 'bar', schemaId: record.schemaId }
     })
     on('http', function(request, response) {
         if (request.operationId === "getPetById") {
             switch (request.path.petId) {
                 case 2:
                     response.statusCode = 404
-                    return true
+                    break
                 case 3:
                     response.statusCode = 404
                     response.data = null
-                    return true
+                    break
                 case 4:
                     response.statusCode = 200
                     response.data = {
 
                     }
-                    return true
+                    break
                 case 5:
                     // use generated data but change pet's name
                     response.data.name = 'Zoe'
-                    return true
+                    break
             }
         }
-        return false
     });
     await kafka.produceAsync({
         topic: 'petstore.order-event',
